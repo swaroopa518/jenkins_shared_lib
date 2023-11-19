6db412d746e1b62142482fb4abc0c9368526be8e
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-19 17:18:44 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -1,10 +1,3 @@
 def call(String jfrogUser){
-    withCredentials([usernamePassword(
-            credentialsId: "jfrog",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-           sh  "curl -X PUT -u $USER -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-           sh "read -p $PASS"           
-        }
+    archiveArtifacts artifacts: '**/target/*.jar', followSymlinks: false
 }
