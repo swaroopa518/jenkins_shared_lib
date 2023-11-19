db930b90b52e9552e06539489b9d0d5e6b06f3b3
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-19 16:43:58 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -4,7 +4,7 @@ def call(String hubUser){
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
-            curl -X POST -u $USER -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar
-            read -p $PASS           
+           sh  'curl -X PUT -u $USER -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+           sh 'read -p $PASS'           
         }
 }
