ae28f042ae25c9f98666121bead1e9337616b0f0
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-19 16:33:35 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -1,5 +1,11 @@
-def call(credentialsId){
-        withSonarQubeEnv(credentialsId: credentialsId) {
-        curl -XPUT-u $uname -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar
-        }
+def call(String hubUser){
+    withCredentials([usernamePassword(
+            credentialsId: "my-artifactory",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+       
+            curl -X PUT -u $USER -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar
+            
     }
+}
