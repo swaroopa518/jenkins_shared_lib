a1af3dac6ce18a10bde2893a90cc9aa726d531c5
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-22 16:49:08 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -1,6 +1,11 @@
-def call(credentialsId){
-
-    withJFrogInstanceEnv(credentialsId: credentialsId) {
+def call(String JfrogUser){
+ withCredentials([usernamePassword(
+            credentialsId: "jfrog",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+   
          archiveArtifacts artifacts: '**/target/*.jar', followSymlinks: false
     }
 }
+
