27fefe0ff5e32b5c35cbbdfa3dc95db7ca1d6455
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-23 11:26:20 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -5,7 +5,7 @@ def call(String JfrogUser){
             passwordVariable: "PASS"
     )]) {
         sh("curl -u $USER -X PUT -T  **/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://<EC2IP>:8082/artifactory/example-repo-local/
-           
+        read -s $PASS
     }
 }
 
