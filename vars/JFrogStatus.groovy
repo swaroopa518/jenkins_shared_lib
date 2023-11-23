b33d457a58284f8208d6395048108ecd9140d5d1
swaroopa518 <84654533+swaroopa518@users.noreply.github.com>
2023-11-23 12:58:47 +0530

    Update JFrogStatus.groovy

--- vars/JFrogStatus.groovy
+++ vars/JFrogStatus.groovy
@@ -1,10 +1,6 @@
-def call(String JfrogUser){
- withCredentials([usernamePassword(
-            credentialsId: "jfrog",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        curl -u '$USER' -X PUT -T  */target/*.jar 'http://54.209.136.136:8082/artifactory/example-repo-local/'
-    }
+def call(){
+ 
+  archiveArtifacts artifacts: '**/target/*.jar', followSymlinks: false
+  
 }
 
